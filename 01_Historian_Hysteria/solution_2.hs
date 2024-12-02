361c8f2ec60f66292a01ab9c1b526f3cc378dcc3
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2024-12-02 10:57:12 +0100

    Day 01: solve part 2

--- 01_Historian_Hysteria/solution_2.hs
+++ 01_Historian_Hysteria/solution_2.hs
@@ -1,2 +1,15 @@
+import Data.Map (fromListWith, findWithDefault)
+
+columns :: String -> ([Int], [Int])
+columns = transposePair .  map (toPair . map read . words) . lines
+  where transposePair = (,) <$> map fst <*> map snd
+        toPair [a,b]  = (a,b)
+        toPair xs     = error $ "invalid data: " ++ show xs
+
+similarityScore :: [Int] -> [Int] -> Int
+similarityScore xs = sum . map singleScore
+  where singleScore x = x * findWithDefault 0 x (summarize xs)
+        summarize     = fromListWith (+) . map (,1)
+
 main :: IO ()
-main = interact $ unlines . map ("Hello, " ++) . lines
+main = interact $ show . uncurry (flip similarityScore) . columns
