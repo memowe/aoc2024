68ad1e26c525e468f13d9ca7485e67fd89ea3a3a
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2024-12-02 10:30:11 +0100

    Day 01: solve part 1

--- 01_Historian_Hysteria/solution_1.hs
+++ 01_Historian_Hysteria/solution_1.hs
@@ -1,2 +1,17 @@
+import Data.List
+import Data.Function
+
+columns :: String -> ([Int], [Int])
+columns = transposePair .  map (toPair . map read . words) . lines
+  where transposePair = (,) <$> map fst <*> map snd
+        toPair [a,b]  = (a,b)
+        toPair xs     = error $ "invalid data: " ++ show xs
+
+distance :: [Int] -> [Int] -> [Int]
+distance = zipWith (\a b -> abs (a-b)) `on` sort
+
+reportDistances :: [Int] -> String
+reportDistances = show . sum
+
 main :: IO ()
-main = interact $ unlines . map ("Hello, " ++) . lines
+main = interact $ reportDistances . uncurry distance . columns
